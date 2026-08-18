6cef3e80634f0fc2ad7a510edff777d1c51bfb5f
andrea8980 <andrea@casaloda.it>
2026-08-18 14:33:27 +0200

    feat: add hero image container and update layout for improved styling

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,11 +4,12 @@
 
 @section('content')
 
+<div class="hero-image-container">
+    <img src="{{ asset('img/jumbotron.jpg') }}" alt="Jumbotron" class="img-fluid" />
+</div>
 
-<img src="{{ asset('img/jumbotron.jpg') }}" alt="Jumbotron" class="img-fluid" />
 
-
-<div class="row g-3 d-flex justify-content-center my-3 bg-black">
+<div class="row g-3 d-flex justify-content-center my-3 bg-black py-4">
     @foreach ($comics as $comic)
     <div class="col-6 col-md-4 col-lg-2 d-flex justify-content-center">
         @include('partials.card', ['comic' => $comic])
